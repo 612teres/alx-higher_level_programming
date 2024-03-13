5579949c42a0295e08b86cfbc464a64b6c9e6f5c
612teres <612teres@gmail.com>
2024-03-13 13:00:51 +0000

    write python

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -2,7 +2,8 @@
 import sys
 
 def main():
-sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
-sys.exit(1)
+	sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+	sys.exit(1)
+
 if __name__ == "__main__":
-main()
+	main()
