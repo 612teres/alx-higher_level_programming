2902c16fa94b5b5438691b1f988faa63eacd8b6f
612teres <612teres@gmail.com>
2024-03-15 07:40:18 -0700

    combinations

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
-for i in range(10):
-    for j in range(i+1, 10):
-        print("{:02d}".format(i), "{:02d}".format(j), sep=", ",/
-                end=", " if i == 8 or j == 9 else "\n")
+for i in range(0, 9):
+    for j in range(i + 1, 10):
+        if i == 8:
+            print("{}{}".format(i, j)
+                    else:
+                    print("{}{}".format(i, j), end=", ")
