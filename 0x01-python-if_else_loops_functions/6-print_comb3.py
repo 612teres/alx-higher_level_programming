06f24c17f8e4bdfa9c2b1690c27e9b7d0fc9a9f5
612teres <612teres@gmail.com>
2024-03-15 07:12:15 -0700

    combimations

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 
 for i in range(0, 9):
     for j in range(i + 1, 10):
-        if i == 8
+        if i != 8 or j != 9
         print("{}{}".format(i, j))
     else:
         print("{}{}".format(i, j))
