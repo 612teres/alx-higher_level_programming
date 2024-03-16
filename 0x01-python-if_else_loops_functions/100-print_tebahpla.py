e8d16a39f716e59e7b4701a14d261b71b4f7f45a
612teres <612teres@gmail.com>
2024-03-16 06:48:36 -0700

    tebalpha

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
-for c in range(ord('z'), ord('a') - 1, - 2):
-
-    print("{:c}{:s}".format(c, chr(c - 33)), end="")
+for c in range(ord('Z'), ord('A') - 1, - 1):
+    c += chr('')
+    c += chr(ord('z') - (c - ord('Z'))
+            print("{:c}".format(c), end="")
