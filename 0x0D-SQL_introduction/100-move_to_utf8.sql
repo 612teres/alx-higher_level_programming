81bef1731bcbcf2f122daa7697a6c486f7251bf7
612teres <612teres@gmail.com>
2024-03-26 11:26:34 -0700

    Even more on databases

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,5 @@
 -- converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
 
 USE hbtn_0c_0;
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8md4 COLLATE utf8md4_unicode_ci;
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8md4 COLLATE utf8md4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8md4, collate utf8md4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mdd4, collate utf8md4_unicode_ci;
