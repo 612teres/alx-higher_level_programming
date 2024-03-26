d684d75ac58565f6d4f2787b2117fe8189b63c0c
612teres <612teres@gmail.com>
2024-03-26 09:37:07 -0700

    I am starting on datatbases

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,2 @@
 -- creates  first_table in mysql server database
-CREATE TABLE IF NOT EXISTS first_table (id INT,
-name ARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (id INT, name ARCHAR(256));
