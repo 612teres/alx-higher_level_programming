0f8ba52a7823b482f538eb9b08d760b7da817b11
612teres <612teres@gmail.com>
2024-04-23 05:38:27 +0000

    warm up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < array.length; i++) {
-  console.log(array[i]);
+let multi = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < multi.length; i++) {
+  console.log(multi[i]);
 }
