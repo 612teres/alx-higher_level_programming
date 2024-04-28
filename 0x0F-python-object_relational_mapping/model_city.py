4732803ec42ea259d7c36cc9d5f2d2666659a175
612teres <612teres@gmail.com>
2024-04-28 13:13:07 -0700

    Python

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Definition of the City class"""
 
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
+from SQLAlchemy.ext.declarative import declarative_base
+from SQLAlchemy import Column, Integer, String, ForeignKey
+from SQLAlchemy.orm import relationship
 
 Base = declarative_base()
 
