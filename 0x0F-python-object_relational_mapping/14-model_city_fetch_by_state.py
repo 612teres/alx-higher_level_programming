e4cef9f7ee8912e21a9052565fbaebaf2f8ae656
612teres <612teres@gmail.com>
2024-04-28 13:01:15 -0700

    Python

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -7,9 +7,9 @@ if __name__ == "__main__":
     import sys
     from model_state import Base, State
     from model_city import City
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-    from sqlalchemy.schema import Table
+    from SQLAlchemy import create_engine
+    from SQLAlchemy.orm import Session
+    from SQLAlchemy.schema import Table
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2],
